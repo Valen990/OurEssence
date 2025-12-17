2e1f8ef4d888aa236d41bbd94bccec615edb4ddc
Valen Torres <valentina.torres990@pascualbravo.edu.co>
2025-12-17 00:02:59 -0500

    Cositas finales por este año

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\MyControllers\TablaController;
 use App\Http\Controllers\MyControllers\PreguntasController;
 use App\Http\Controllers\MyControllers\InformacionController;
 
-Route::get('/Inicio', [InicioController::class, 'index']);
-Route::get('/Registro', [RegistroController::class, 'register']);
-Route::get('/Registros', [TablaController::class, 'table']);
-Route::get('/Preguntas', [PreguntasController::class, 'questions']);
-Route::get('/Informacion', [InformacionController::class, 'information']);
+Route::get('/inicio', [InicioController::class, 'index']);
+Route::get('/registro', [RegistroController::class, 'register']);
+Route::get('/registros', [TablaController::class, 'table']);
+Route::get('/preguntas', [PreguntasController::class, 'questions']);
+Route::get('/informacion', [InformacionController::class, 'information']);
